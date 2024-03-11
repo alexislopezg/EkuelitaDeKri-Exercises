4f451947bfd6222f351cf25fc7acda2fd486230c
Alexis L <alexislopezg@pm.me>
2024-03-11 18:07:17 -0400

    StackingToy class exercise

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,17 +9,25 @@ public class Main {
 //        lightBulb.turnOff();
 //        lightBulb.turnOff();
 
-        var tweet = new Tweet("Me", "This is a test tweet");
-        System.out.println(tweet.getMessage());
-        System.out.println(tweet.getAuthor());
-        System.out.println(tweet.getCreatedDate());
+//        var tweet = new Tweet("Me", "This is a test tweet");
+//        System.out.println(tweet.getMessage());
+//        System.out.println(tweet.getAuthor());
+//        System.out.println(tweet.getCreatedDate());
+//
+//        tweet.like();
+//
+//        tweet.dislike();
+//        tweet.dislike();
+//        tweet.dislike();
+//
+//        System.out.println("Total like count is: " + tweet.getLikeCount());
 
-        tweet.like();
+        var stackingToy = new StackingToy();
+        stackingToy.addRing("Blue");
+        stackingToy.addRing("Yellow");
+        stackingToy.addRing("Red");
 
-        tweet.dislike();
-        tweet.dislike();
-        tweet.dislike();
+        stackingToy.removeRing();
 
-        System.out.println("Total like count is: " + tweet.getLikeCount());
     }
 }
---
+++ src/main/java/StackingToy.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class StackingToy {
+
+    private List<String> stackedRings = new ArrayList<>();
+
+    public void addRing(String ring) {
+        stackedRings.add(ring);
+
+
+        for (var addedRings : stackedRings) {
+            System.out.println(addedRings);
+        }
+    }
+
+    public void removeRing() {
+        System.out.println("Removed ring: " + stackedRings.getLast());
+        stackedRings.removeLast();
+
+        for (var addedRings : stackedRings) {
+            System.out.println(addedRings);
+        }
+    }
+}
